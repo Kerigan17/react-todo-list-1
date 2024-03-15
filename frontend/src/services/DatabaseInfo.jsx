b8f48cb4c4c41b8b61c8d30ad2ced4554e17cf96
Alexander Malko <alexsandr77867@gmail.com>
2024-03-15 13:26:33 +0400

    create method addTask

---
+++ frontend/src/services/DatabaseInfo.jsx
@@ -0,0 +1,19 @@
+const defaultUrl = 'http://localhost:8800/';
+export const getInfo = (url) =>
+    fetch(defaultUrl + url)
+        .then(response => response.json())
+        .then(data =>{return data})
+        .catch(error => console.error(error));
+
+export const postInfo = (url, task) => fetch(defaultUrl + url, {
+    method: 'POST',
+    headers: {'Content-Type' : 'application/json'},
+    body: JSON.stringify({task: task, completed: false})
+})
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+
+export const delInfo = (url, id) => fetch(defaultUrl + url, {
+
+})
